b7199e29540e788b41832a2bd861b17bb6ba89e2
yangshuai <yangshuai@pachiratech.com>
2017-09-27 13:42:38 +0800

    Modify some notes.

--- src/Seq2SeqModel.py
+++ src/Seq2SeqModel.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from __future__ import print_function
 
 import logging
@@ -7,8 +8,7 @@ import h5py
 from tensorflow.contrib import rnn
 from DataSet import DataSet
 
-# Config the logger.
-# Output into log file.
+''' Config the logger, output into log file.'''
 log_file_name = "log/model.log"
 if not os.path.exists(log_file_name):
     f = open(log_file_name, 'w')
@@ -18,31 +18,33 @@ logging.basicConfig(level = logging.DEBUG,
                 datefmt='%a, %d %b %Y %H:%M:%S',
                 filename=log_file_name,
                 filemode='w')
-# Output to the console.
+
+''' Output to the console.'''
 console = logging.StreamHandler()
 console.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
 console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 
-# 设置 GPU 按需增长
+''' GPU  setting'''
 config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.8
 
+''' HDF5 file (data) info.'''
 hdf5DirPath = "data/hdf5/"
 hdf5Filename = "APLAWDW_s_01_a"
 hdf5Extension = ".hdf5"
-
 dataFile = h5py.File(hdf5DirPath + hdf5Filename + hdf5Extension)
 
-# 学习率
+'''Learning model parameters'''
 learningRate = 1e-3
-# 在训练和测试的时候，我们想用不同的 batch_size.所以采用占位符的方式
-# batchSize = tf.placeholder(tf.int32)  # 注意类型必须为 tf.int32
 batchSize = 8 # During an iteration, each batch need memory space around 1Gb.
-#  training iteration
+#  Total training iteration
 iteration = 500
+#  After a fixed count of iteration, display some info(eg. accuracy) about training.
+displayIteration = 5
 
+'''Data info'''
 # 每个时刻的输入特征是40000维的，就是每个时刻输入一行，一行有 1 个像素
 inputSize = 1
 # 时序持续长度为40000，即每做一次预测，需要先输入40000行
@@ -54,11 +56,11 @@ layerNum = 1
 # 最后输出分类类别数量，如果是回归预测的话应该是 1
 classNum = 1
 
+'''DNN model'''
 X = tf.placeholder(tf.float32, [batchSize, timestepSize, inputSize])
 y = tf.placeholder(tf.float32, [batchSize, timestepSize, classNum])
 keep_prob = tf.placeholder(tf.float32)
 
-#
 with tf.variable_scope("LSTM") as vs:
     # Define parameters of full connection between the second LSTM layer and output layer.
     # Define weights.
@@ -95,6 +97,7 @@ with tf.variable_scope("LSTM") as vs:
     correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
+    '''Start a session and run up.'''
     with tf.Session(config=config) as sess:
         logging.info("Session started!")
         sess.run(tf.global_variables_initializer())
@@ -103,9 +106,8 @@ with tf.variable_scope("LSTM") as vs:
         for i in range(iteration):
             logging.info("Iteration: "+ str(i))
             (batchX, batchY) = dataSet.getBatch(i)
-            if (i+1)% 5 == 0:
+            if (i+1)% displayIteration == 0:
                 train_accuracy = sess.run(accuracy, feed_dict={X:batchX, y: batchY, keep_prob: 1.0})
-                # 已经迭代完成的 epoch 数
                 logging.info("Epoch:%d,\t iteration:%d,\t training accuracy:%g" % ( dataSet.completedEpoch, (i+1), train_accuracy))
                 pass
             sess.run(train_op, feed_dict={X:batchX, y: batchY, keep_prob: 1.0})
