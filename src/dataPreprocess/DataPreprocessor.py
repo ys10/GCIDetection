159147db5a974abc6b7fa9580349d0dc0b6beb85
yangshuai <yangshuai@pachiratech.com>
2017-10-19 17:05:41 +0800

    Data preprocessor provides for data framing.

--- src/dataPreprocess/DataPreprocessor.py
+++ src/dataPreprocess/DataPreprocessor.py
@@ -3,7 +3,7 @@ import wave
 import numpy
 import os
 import logging
-from math import floor
+from math import floor, ceil
 
 ''' Config the logger, output into log file.'''
 log_file_name = "log/dataProcess.log"
@@ -25,9 +25,10 @@ logging.getLogger('').addHandler(console)
 
 
 class DataPreprocessor(object):
-    def __init__(self, dataFilePath, waveDirPath="data/wav/", waveExtension=".wav", markDirPath="data/mark/",
+    def __init__(self, dataFilePath, framSize=1, waveDirPath="data/wav/", waveExtension=".wav", markDirPath="data/mark/",
                  markExtension=".mark"):
         self.dataFilePath = dataFilePath
+        self.frameSize = framSize
         self.waveDirPath = waveDirPath
         self.markDirPath = markDirPath
         self.waveExtension = waveExtension
@@ -46,6 +47,20 @@ class DataPreprocessor(object):
         logging.debug("All files name: " + str(keyList))
         return keyList
 
+    def __getDataType(self, sampleWidth):
+        if(sampleWidth == 2):
+            dataType = numpy.short
+        else:
+            dataType = numpy.int32
+        return dataType
+
+    def __padWave(self, waveData, dataType):
+        length = int(ceil(waveData.__len__()/self.frameSize) * self.frameSize)
+        logging.debug("length after padding:" + str(length))
+        paddedData = numpy.zeros(shape = (length,), dtype=dataType)
+        paddedData[:waveData.__len__()] = waveData
+        return paddedData
+
     def __getLocations(self, markFile):
         locations = list()
         while 1:
@@ -59,13 +74,14 @@ class DataPreprocessor(object):
         return locations
 
     # Transform GCI locations to label(binary classification) sequence.
-    def __transLocations2LabelSeq(self, locations, labelSeqLength, samplingRate):
+    def __transLocations2LabelSeq(self, locations, nSamplingPoints, samplingRate):
+        labelSeqLength = ceil(nSamplingPoints / self.frameSize)
         zero = numpy.zeros(shape=(labelSeqLength, 1), dtype=numpy.float32)
         one = numpy.ones(shape=(labelSeqLength, 1), dtype=numpy.float32)
         labelSeq = numpy.reshape(numpy.asarray([zero, one]).transpose(), [labelSeqLength, 2])
         logging.debug("mark data shape:" + str(labelSeq.shape))
         for location in locations:
-            labelLocation = floor(int(location * samplingRate)) - 1
+            labelLocation = floor(int(location * samplingRate / self.frameSize)) - 1
             logging.debug("Time:" + str(labelLocation))
             labelSeq[labelLocation][0] = 1.0
             labelSeq[labelLocation][1] = 0.0
@@ -82,14 +98,16 @@ class DataPreprocessor(object):
                 # Read wave params & data
                 with wave.open(self.waveDirPath + fileName + self.waveExtension, "rb") as waveFile:
                     params = waveFile.getparams()
-                    nchannels, sampwidth, framerate, nframes = params[:4]
-                    logging.debug("\tnchannels:\t" + str(nchannels))
-                    logging.debug("\tsampwidth:\t" + str(sampwidth))
-                    logging.debug("\tframerate:\t" + str(framerate))
-                    logging.debug("\tnframes:\t" + str(nframes))
-                    strData = waveFile.readframes(nframes)
-                    waveData = numpy.fromstring(strData, dtype=numpy.short)
-                    waveData = numpy.reshape(waveData, (nframes, 1))
+                    nChannels, sampleWidth, samplingRate, nSamplingPoints = params[:4]
+                    logging.debug("\tnChannels:\t" + str(nChannels))
+                    logging.debug("\tsampleWidth:\t" + str(sampleWidth))
+                    logging.debug("\tsamplingRate:\t" + str(samplingRate))
+                    logging.debug("\tnSamplingPoints:\t" + str(nSamplingPoints))
+                    strWaveData = waveFile.readframes(nSamplingPoints)
+                    dataType = self.__getDataType(sampleWidth)
+                    waveData = numpy.fromstring(strWaveData, dtype=dataType)
+                    waveData = self.__padWave(waveData, dataType)
+                    waveData = numpy.reshape(waveData, (ceil(nSamplingPoints/self.frameSize), self.frameSize))
                     logging.debug("\twave data shape:\t" + str(waveData.shape))
                     # write wave date into hdf5 file.
                     h5File[fileName + "/input"] = list(waveData.astype(numpy.float32))
@@ -97,9 +115,8 @@ class DataPreprocessor(object):
                 # Read GCI locations.
                 with open(self.markDirPath + fileName + self.markExtension) as markFile:
                     locations = self.__getLocations(markFile)
-                    # Initialize mark data with all zero list whose length equals to wave data
-                    labelSeq = self.__transLocations2LabelSeq(locations, nframes, framerate)
-                    # write mark date into hdf5 file.
+                    labelSeq = self.__transLocations2LabelSeq(locations, nSamplingPoints, samplingRate)
+                    # write label date into hdf5 file.
                     h5File[fileName + "/label"] = labelSeq
                     pass  # markFile close
                 pass  #
@@ -111,7 +128,7 @@ class DataPreprocessor(object):
 
 def main():
     dataFilePath = "data/hdf5/APLAWDW_test.hdf5"
-    dataPreprocessor = DataPreprocessor(dataFilePath)
+    dataPreprocessor = DataPreprocessor(dataFilePath, framSize=1)
     dataPreprocessor.process()
     pass
 
