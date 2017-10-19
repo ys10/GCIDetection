3ac5e5bc0f6117c6c2821e9371207bfcce6e2d80
yangshuai <yangshuai@pachiratech.com>
2017-10-19 14:03:30 +0800

    Add transform function.

--- src/ResultWriter.py
+++ src/ResultWriter.py
@@ -1,12 +1,26 @@
+import numpy as np
+
 class ResultWriter(object):
-    def __init__(self, resultFile):
+
+    def __init__(self, resultFile, samplingRate):
         self.resultFile = resultFile
+        self.samplingRate = samplingRate
         pass
 
     def saveBatchResult(self, batchResult, keyList):
         for i in range(keyList.__len__()):
-            self.resultFile[keyList[i]] = batchResult[i]
+            self.resultFile[keyList[i]] = self.transLabelSeq2Locations(batchResult[i], self.samplingRate)
             pass
         pass
+
+    # Transform label(binary classification) sequence to GCI locations
+    def transLabelSeq2Locations(self, labelSeq, samplingRate):
+        locations = list()
+        labelLocations = np.where(np.array(labelSeq)[:, 0]==1)[0].tolist()
+        for labelLocation in labelLocations:
+            location = (labelLocation+1) / samplingRate
+            locations.append(location)
+            pass
+        return locations
 
     pass
