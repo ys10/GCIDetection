ac0618ae2ae41cce92f26ac5f958e0c251743c18
yangshuai <yangshuai@pachiratech.com>
2017-10-22 14:57:37 +0800

    Distinguish regression model from classification model.

---
+++ src/TrainFrame17RegressionModel.py
@@ -0,0 +1,33 @@
+from dnnModel.RegressionModel import  *
+
+
+class TrainFrame17RegressionModel(object):
+    def __init__(self):
+        self.model = RegressionModel(inputSize=17, timeStepSize=None, hiddenSize=256, layerNum=2, classNum=2,
+                                learningRate=1e-3)
+        self.dataFilename = "data/hdf5/APLAWDW_frame_17_regression.hdf5"
+        self.resultFilename = "data/hdf5/APLAWDW_frame_17_regression_result.hdf5"
+        self.modelRestorePath = None
+        self.modelSavePath = "model/APLAWDW_frame_17_regression_model"
+        self.summarySavePath = "summary/APLAWDW_frame_17_regression_model/"
+        pass
+
+    def run(self):
+        self.model.setDataFilename(self.dataFilename, self.resultFilename)
+        self.model.setModelSavePath(self.modelRestorePath, self.modelSavePath)
+        self.model.setSummarySavePath(self.summarySavePath)
+        self.model.train(trainIteration=10000, saveIteration=500, displayIteration=5, batchSize=16, samplingRate=20000)
+        pass
+
+    pass
+
+
+def main():
+    frameModel = TrainFrame17RegressionModel()
+    frameModel.run()
+    pass
+
+
+if __name__ == '__main__':
+    main()
+    pass
