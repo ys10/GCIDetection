d214754481965243024d63d1bbef54d482621fd1
yangshuai <yangshuai@pachiratech.com>
2017-10-26 17:18:28 +0800

    Add validation data preprocessor for frame 9.

--- src/dataPreprocess/ProcessClassificationValidationDataByFrame9.py
+++ src/dataPreprocess/ProcessClassificationValidationDataByFrame9.py
@@ -1,12 +1,12 @@
 from dataPreprocess.ClassificationDataPreprocessor import *
 
-dataFilePath = "data/hdf5/APLAWDW_frame_9_classification_training.hdf5"
+dataFilePath = "data/hdf5/APLAWDW_frame_9_classification_validation.hdf5"
 dataPreprocessor = ClassificationDataPreprocessor(dataFilePath,
                                                   frameSize=9,
                                                   frameStride=9,
-                                                  waveDirPath="data/APLAWDW/wav/",
+                                                  waveDirPath="data/APLAWDW/valid_wav/",
                                                   waveExtension=".wav",
-                                                  markDirPath="data/APLAWDW/mark/",
+                                                  markDirPath="data/APLAWDW/valid_mark/",
                                                   markExtension=".marks")
 dataPreprocessor.setDefaultRadius(defaultRadius = 40)
 dataPreprocessor.process()
