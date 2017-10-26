40765b1ed42b226c507df5e938ab6f041b363e97
yangshuai <yangshuai@pachiratech.com>
2017-10-26 17:05:58 +0800

    Change frame stride from 5 to 9(equals to frame size).

--- src/dataPreprocess/ProcessClassificationDataByFrame9.py
+++ src/dataPreprocess/ProcessClassificationDataByFrame9.py
@@ -3,7 +3,7 @@ from dataPreprocess.ClassificationDataPreprocessor import *
 dataFilePath = "data/hdf5/APLAWDW_frame_9_classification.hdf5"
 dataPreprocessor = ClassificationDataPreprocessor(dataFilePath,
                                                   frameSize=9,
-                                                  frameStride=5,
+                                                  frameStride=9,
                                                   waveDirPath="data/APLAWDW/wav/",
                                                   waveExtension=".wav",
                                                   markDirPath="data/APLAWDW/mark/",
